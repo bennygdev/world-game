df2626e3b82dc1ae73af24f4e2882faf87fd66c9
Benny Goh <haiguan648@gmail.com>
2025-02-20 23:09:31 +0800

    setup

--- README.md
+++
@@ -1,2 +0,0 @@
-# WorldMapGame
-This game is a work in progress, basically i wanted to make a game where it can test your knowledge about the world. soon to be updated
---
+++ client/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+import axios from 'axios';
+
+function App() {
+  const [array, setArray] = useState([]);
+
+  const fetchAPI = async () => {
+    const response = await axios.get('http://localhost:8080/api');
+    setArray(response.data.fruits);
+    console.log(response.data.fruits);
+  };
+
+  useEffect(() => {
+    fetchAPI();
+  }, []);
+
+  return (
+    <>
+      {
+        array.map((fruit, index) => {
+          return (
+            <div key={index}>
+              <p>{fruit}</p>
+              <br />
+            </div>
+          );
+        })
+      }
+    </>
+  )
+}
+
+export default App
